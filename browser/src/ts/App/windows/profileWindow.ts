18b22f2dd1fd92631a6a649321835348363197f6
MoodyDreamer <38349760+ProphetAlgorithms@users.noreply.github.com>
2026-07-30 15:42:36 +0200

    ES compatibility updates

--- browser/src/ts/App/windows/profileWindow.ts
+++ browser/src/ts/App/windows/profileWindow.ts
@@ -1,8 +1,8 @@
 import { Profile, ProfileManager } from "../profileManager";
-import { Messagebox, messagebox } from "../messagebox";
+import { Messagebox, messagebox } from "../messagebox.js";
 import { isNumeric } from "jquery";
 import { WindowManager } from "../windowManager";
-import { circleNavigate } from "../../Core/util";
+import { circleNavigate } from "../../Core/util.js";
 
 export class ProfileWindow {
     public defaultServer = "mud.temporasanguinis.it";
