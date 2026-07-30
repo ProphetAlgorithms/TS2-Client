0eb683a554cef18f313b9ba8562db8b22c5603b9
MoodyDreamer <38349760+ProphetAlgorithms@users.noreply.github.com>
2026-07-30 15:17:39 +0200

    ES compatibility updates

--- browser/tools/genAppInfo.js
+++ browser/tools/genAppInfo.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
+import fs from "fs";
 
-const pjson = require('../package.json');
+import pjson from '../package.json' with { type: 'json' };
 
 
 let build = process.argv[2];
-const { exec } = require("child_process");
-const { exit } = require("process");
+import { exec } from "child_process";
+import { exit } from "process";
 
 exec(build, (error, stdout, stderr) => {
     const buildhash = stdout.trim().toString();
