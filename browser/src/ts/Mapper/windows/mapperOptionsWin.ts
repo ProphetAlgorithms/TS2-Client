f26bc44e0f8ef61e52395fd0ede24222f1339719
MoodyDreamer <38349760+ProphetAlgorithms@users.noreply.github.com>
2026-07-30 15:58:52 +0200

    ES compatibility updates

--- browser/src/ts/Mapper/windows/mapperOptionsWin.ts
+++ browser/src/ts/Mapper/windows/mapperOptionsWin.ts
@@ -1,5 +1,5 @@
 import {ExitDir, Mapper, MapperOptions, Room, RoomExit, Zone} from "../mapper"
-import { circleNavigate, colorCssToRGB, colorToHex, makeIndeterminate } from "../../Core/util";
+import { circleNavigate, colorCssToRGB, colorToHex, makeIndeterminate } from "../../Core/util.js";
 
 export type BooleanFunction = (opt: MapperOptions) => void;
 
@@ -284,4 +284,3 @@ export class MapperOptionsWin {
         (<any>this.$win).jqxWindow("close");
     }
 }
-
