0a357bc9ee7cba9c7e8fe8cd21b1dfa3016e878b
MoodyDreamer <38349760+ProphetAlgorithms@users.noreply.github.com>
2026-07-30 16:01:43 +0200

    ES compatibility updates

--- browser/src/ts/Scripting/aliasManager.ts
+++ browser/src/ts/Scripting/aliasManager.ts
@@ -1,9 +1,9 @@
 import { TrigAlItem } from "./windows/trigAlEditBase";
-import { EventHook } from "../Core/event";
+import { EventHook } from "../Core/event.js";
 import { ClassManager } from "./classManager";
-import { API, EvtScriptEmitPrint, EvtScriptEmitToggleAlias, JsScript } from "./jsScript";
+import { API, EvtScriptEmitPrint, EvtScriptEmitToggleAlias, JsScript } from "./jsScript.js";
 import { ProfileManager } from "../App/profileManager";
-import { ConfigIf, escapeRegExp, parseScriptVariableAndParameters, parseShortcutString, parseSimpleScriptSyntax } from "../Core/util";
+import { ConfigIf, escapeRegExp, parseScriptVariableAndParameters, parseShortcutString, parseSimpleScriptSyntax } from "../Core/util.js";
 import { UserConfig } from "../App/userConfig";
 import hotkeys from 'hotkeys-js';
 
